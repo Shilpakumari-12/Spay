e23ce00db004cb4af1009d8b0cf4451835312556
Shilpa Kumari <145902859+Shilpakumari-12@users.noreply.github.com>
2024-04-07 04:41:05 +0530

    Added utils

---
+++ src/Utils/Utils/networks.js
@@ -0,0 +1,37 @@
+export const chainIdMapping = {
+    "0x1":{
+        name:"Ethereum",
+        rpcUrls: ["https://mainnet.infura.io/v3/"],
+        blockExplorerUrls: ["https://etherscan.io"],
+        nativeCurrency: { name: 'ETH', decimals: 18, symbol: 'ETH' },
+        hex: "0x1"
+    },
+    "0x89": {
+        name: "Polygon",
+        rpcUrls: ["https://polygon-rpc.com/"],
+        blockExplorerUrls: ["https://polygonscan.com/"],
+        nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+        hex: "0x89"
+    },
+    "0x13881": {
+        name: "Mumbai",
+        rpcUrls: ["https://mumbai.rpc.thirdweb.com/ed043a51ae23b0db3873f5a38b77ab28175fa496f15d3c53cf70401be89b622a"],
+        blockExplorerUrls: ["https://mumbai.polygonscan.com"],
+        nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+        hex: "0x13881"
+    },
+    137:{
+        name: "Polygon",
+        rpcUrls: ["https://polygon-rpc.com/"],
+        blockExplorerUrls: ["https://polygonscan.com/"],
+        nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+        hex: "0x89"
+    },
+    80001:{
+        name: "Mumbai",
+        rpcUrls: ["https://mumbai.rpc.thirdweb.com/ed043a51ae23b0db3873f5a38b77ab28175fa496f15d3c53cf70401be89b622a"],
+        blockExplorerUrls: ["https://mumbai.polygonscan.com"],
+        nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+        hex:"0x13881"
+    }
+}
